2eddd9dc49cc04704a61059b2828584cf9e16370
rrunner <therainbowrunner@gmail.com>
2024-07-23 08:58:47 +0200

    fix lazydev (include a dependency)

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -195,6 +195,11 @@ return {
       },
       sources = {
         {
+          name = "lazydev",
+          -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+          group_index = 0,
+        },
+        {
           name = "nvim_lsp",
           entry_filter = function(entry, _)
             return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
--- nvim/.config/nvim/lua/plugins/lazydev.lua
+++ nvim/.config/nvim/lua/plugins/lazydev.lua
@@ -1,8 +1,15 @@
--- configure lua_ls for editing your nvim config
+-- `lazydev` configures Lua LSP for the nvim config, runtime and plugins
 return {
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {},
+  "folke/lazydev.nvim",
+  ft = "lua",
+  dependencies = {
+    "Bilal2453/luvit-meta",
+    lazy = true,
+  },
+  opts = {
+    library = {
+      -- load luvit types when the `vim.uv` word is found
+      { path = "luvit-meta/library", words = { "vim%.uv" } },
+    },
   },
 }
