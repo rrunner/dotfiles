d55d90ed5d481fc1138483f76f0970d93784bf0a
rrunner <therainbowrunner@gmail.com>
2024-08-25 23:00:35 +0200

    update gp plugin

--- nvim/.config/nvim/lua/plugins/gp.lua
+++ nvim/.config/nvim/lua/plugins/gp.lua
@@ -14,31 +14,15 @@ return {
     require("gp").setup({
       curl_params = curl, -- curl_params = { "--proxy", "http://X.X.X.X:XXXX" }
       chat_user_prefix = "human>",
-      chat_topic_gen_model = "gpt-3.5-turbo-0125",
+      chat_topic_gen_model = "gpt-4o",
       toggle_target = "vsplit",
       agents = {
         {
           name = "ChatGPT4",
           chat = true,
-          command = false,
+          command = true,
           model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
           -- system prompt (use this to specify the persona/role of the AI)
-          system_prompt = "You are a general AI assistant.\n\n"
-            .. "The user provided the additional info about how they would like you to respond:\n\n"
-            .. "- If you're unsure don't guess and say you don't know instead.\n"
-            .. "- Ask question if you need clarification to provide better answer.\n"
-            .. "- Think deeply and carefully from first principles step by step.\n"
-            .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-            .. "- Use Socratic method to improve your thinking and coding skills.\n"
-            .. "- Don't elide any code from your output if the answer requires coding.\n"
-            .. "- Take a deep breath; You've got this!\n",
-        },
-        {
-          name = "ChatGPT3-5",
-          chat = true,
-          command = true,
-          model = { model = "gpt-3.5-turbo-0125", temperature = 1.1, top_p = 1 },
-          -- specify the persona/role of the AI
           system_prompt = "You are a general AI assistant.\n\n"
             .. "The user provided the additional info about how they would like you to respond:\n\n"
             .. "- If you're unsure don't guess and say you don't know instead.\n"
