b3d1449415798cc09dc382df6a5930572405c1cb
rrunner <therainbowrunner@gmail.com>
2024-09-15 19:15:07 +0200

    add various requirements files to be considered filetype requirements

--- nvim/.config/nvim/lua/config/filetype.lua
+++ nvim/.config/nvim/lua/config/filetype.lua
@@ -2,4 +2,8 @@ vim.filetype.add({
   extension = {
     hurl = "hurl",
   },
+  filename = {
+    ["requirements-dev.txt"] = "requirements",
+    ["requirements_dev.txt"] = "requirements",
+  },
 })
