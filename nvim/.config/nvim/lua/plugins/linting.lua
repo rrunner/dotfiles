baa88a8b85b44a77e0b9255af5b5e80830fce5a6
rrunner <therainbowrunner@gmail.com>
2024-10-24 23:31:11 +0200

    configure mypy via nvim-lint and remove settings from pyproject.toml

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -5,50 +5,8 @@ return {
     local lint = require("lint")
     local utils = require("config.utils")
 
-    -- modify linters
-
-    -- ruff updates for all events specified in the autocmd below
-    -- local function get_file_name()
-    --   return vim.api.nvim_buf_get_name(0)
-    -- end
-
-    -- lint.linters.ruff.cmd = utils.app_prio("ruff")
-
-    -- lint.linters.ruff.args = {
-    --   "check",
-    --   "--select=ALL", -- aggressive setting
-    --   -- RET504: unnecessary assignment before return statement
-    --   -- PLR2004: use of magic constant values
-    --   -- PD901: use of name df with pandas
-    --   -- PLR0913: more than 5 arguments to a function
-    --   "--ignore=D407,T201,ERA,D406,RET504,PLR2004,PD901,PLR0913",
-    --   "--force-exclude",
-    --   "--quiet",
-    --   "--stdin-filename",
-    --   get_file_name,
-    --   "--no-fix",
-    --   "--output-format",
-    --   "json",
-    --   "-",
-    -- }
-
     -- mypy only works with stdin = false (only updates for event BufWritePost in the autocmd below)
     lint.linters.mypy.cmd = utils.app_prio("mypy")
-    lint.linters.mypy.args = {
-      "--strict", -- aggressive setting
-      "--ignore-missing-imports",
-      "--follow-imports=silent",
-      "--show-column-numbers",
-      "--show-error-end",
-      "--hide-error-context",
-      "--no-color-output",
-      "--no-error-summary",
-      "--no-pretty",
-      "--python-executable",
-      function()
-        return vim.fn.exepath("python3") or vim.fn.exepath("python")
-      end,
-    }
 
     -- sqlfluff
     -- different sql dialects:
@@ -60,8 +18,7 @@ return {
     lint.linters.sqlfluff.args = { "lint", "--dialect", "postgres" }
 
     lint.linters_by_ft = {
-      -- use ruff LSP for python linting to enable config via pyproject.toml
-      python = { "mypy" },
+      python = { "mypy" }, -- use ruff LSP for python linting to enable config via pyproject.toml
       sql = { "sqlfluff" },
       mysql = { "sqlfluff" },
       markdown = { "markdownlint-cli2" },
