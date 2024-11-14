b968517f572ce0adcca1365c90f078d19affe1db
rrunner <therainbowrunner@gmail.com>
2024-11-14 21:56:00 +0100

    update foldexpr

--- nvim/.config/nvim/lua/config/folds.lua
+++ nvim/.config/nvim/lua/config/folds.lua
@@ -2,48 +2,16 @@
 
 M = {}
 
-M.skip_foldexpr = {} ---@type table<number,boolean>
-
-local skip_check = assert(vim.uv.new_check())
-
 function M.foldexpr()
   local buf = vim.api.nvim_get_current_buf()
-
-  -- no highlight, no foldexpr
-  if not vim.b[buf].ts_highlight then
-    return "0"
+  if vim.b[buf].ts_folds == nil then
+    -- treesitter is not available if there is no filetype
+    if vim.bo[buf].filetype == "" then
+      return "0"
+    end
+    vim.b[buf].ts_folds = pcall(vim.treesitter.get_parser, buf)
   end
-
-  -- still in the same tick and no parser
-  if M.skip_foldexpr[buf] then
-    return "0"
-  end
-
-  -- don't use treesitter folds for terminal
-  if vim.bo[buf].buftype == "terminal" then
-    return "0"
-  end
-
-  -- as long as we don't have a filetype, don't bother
-  -- checking if treesitter is available (it won't)
-  if vim.bo[buf].filetype == "" then
-    return "0"
-  end
-
-  local ok = pcall(vim.treesitter.get_parser, buf)
-
-  if ok then
-    return vim.treesitter.foldexpr()
-  end
-
-  -- no parser available, so mark it as skip
-  -- in the next tick, all skip marks will be reset
-  M.skip_foldexpr[buf] = true
-  skip_check:start(function()
-    M.skip_foldexpr = {}
-    skip_check:stop()
-  end)
-  return "0"
+  return vim.b[buf].ts_folds and vim.treesitter.foldexpr() or "0"
 end
 
 return M
