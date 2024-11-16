119889b721a695533f3d46ec4b00038b003d18fc
rrunner <therainbowrunner@gmail.com>
2024-11-16 14:32:53 +0100

    use more of snacks default settings

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -8,16 +8,14 @@ return {
       bigfile = {
         enabled = true,
         notify = true,
-        size = 1.5 * 1024 * 1024,
+        -- size = 1.5 * 1024 * 1024,
       },
       notifier = {
         enabled = true,
         timeout = 3000,
-        width = { min = 25, max = 0.25 },
-        height = { min = 5, max = 0.5 },
+        width = { min = 30, max = 30 },
+        height = { min = 5, max = 0.3 },
         margin = { top = 0, right = 0, bottom = 0 },
-        padding = true,
-        sort = { "added", "level" },
         icons = icons.snacks,
         style = "fancy",
       },
@@ -35,13 +33,11 @@ return {
       },
       styles = {
         notification = {
-          border = "rounded",
           wo = {
             winblend = 0,
             wrap = true,
           },
           history = {
-            border = "rounded",
             width = 0.9,
             height = 0.9,
             keys = { q = "close" },
