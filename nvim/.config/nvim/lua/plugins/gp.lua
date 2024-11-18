a2658df89b3b0288ebe4f6bf7b2076e26b8f3b28
rrunner <therainbowrunner@gmail.com>
2024-11-18 22:23:56 +0100

    update gp config

--- nvim/.config/nvim/lua/plugins/gp.lua
+++ nvim/.config/nvim/lua/plugins/gp.lua
@@ -16,6 +16,12 @@ return {
       chat_user_prefix = "human>",
       chat_topic_gen_model = "gpt-4o",
       toggle_target = "vsplit",
+      providers = {
+        openai = {
+          endpoint = "https://api.openai.com/v1/chat/completions",
+          secret = os.getenv("OPENAI_API_KEY"),
+        },
+      },
       agents = {
         {
           name = "ChatGPT4",
@@ -23,15 +29,7 @@ return {
           command = true,
           model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
           -- system prompt (use this to specify the persona/role of the AI)
-          system_prompt = "You are a general AI assistant.\n\n"
-            .. "The user provided the additional info about how they would like you to respond:\n\n"
-            .. "- If you're unsure don't guess and say you don't know instead.\n"
-            .. "- Ask question if you need clarification to provide better answer.\n"
-            .. "- Think deeply and carefully from first principles step by step.\n"
-            .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-            .. "- Use Socratic method to improve your thinking and coding skills.\n"
-            .. "- Don't elide any code from your output if the answer requires coding.\n"
-            .. "- Take a deep breath; You've got this!\n",
+          system_prompt = require("gp.defaults").chat_system_prompt,
         },
       },
       hooks = {
@@ -39,28 +37,26 @@ return {
           local template = "I have the following code from {{filename}}:\n\n"
             .. "```{{filetype}}\n{{selection}}\n```\n\n"
             .. "Please respond by writing table driven unit tests for the code above."
-            .. "Please prefer pytest for python code and testthat for R code."
           local agent = gp.get_command_agent()
-          gp.Prompt(params, gp.Target.enew, nil, agent.model, template, agent.system_prompt)
+          gp.Prompt(params, gp.Target.vnew, agent, template)
         end,
         Explain = function(gp, params)
           local template = "I have the following code from {{filename}}:\n\n"
             .. "```{{filetype}}\n{{selection}}\n```\n\n"
             .. "Please respond by explaining the code above."
           local agent = gp.get_chat_agent()
-          gp.Prompt(params, gp.Target.popup, nil, agent.model, template, agent.system_prompt)
+          gp.Prompt(params, gp.Target.popup, agent, template)
         end,
         CodeReview = function(gp, params)
           local template = "I have the following code from {{filename}}:\n\n"
             .. "```{{filetype}}\n{{selection}}\n```\n\n"
             .. "Please analyze for code smells and suggest improvements."
           local agent = gp.get_chat_agent()
-          gp.Prompt(params, gp.Target.enew("markdown"), nil, agent.model, template, agent.system_prompt)
+          gp.Prompt(params, gp.Target.enew("markdown"), agent, template)
         end,
         Translator = function(gp, params)
-          local agent = gp.get_command_agent()
-          local chat_system_prompt = "You are a Translator, please translate between English and Swedish."
-          gp.cmd.ChatNew(params, agent.model, chat_system_prompt)
+          local chat_system_prompt = "You are a Translator, please translate between English and Chinese."
+          gp.cmd.ChatNew(params, chat_system_prompt)
         end,
         BufferChatNew = function(gp, _)
           -- call GpChatNew command in range mode on whole buffer
