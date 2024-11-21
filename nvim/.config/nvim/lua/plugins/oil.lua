37479bf59c5e7c3860284fe33c28db1694e8ac1b
rrunner <therainbowrunner@gmail.com>
2024-11-21 22:59:49 +0100

    update oil config

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -4,25 +4,29 @@ return {
     event = "BufEnter",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("oil").setup({
+      local oil = require("oil")
+      oil.setup({
         columns = { "icon" },
-        use_default_keymaps = true,
+        use_default_keymaps = false,
         keymaps = {
-          ["q"] = "actions.close",
+          ["q"] = {
+            function()
+              oil.close({ exit_if_last_buf = true })
+            end,
+            opts = {},
+            desc = "Close oil buffer (also nvim if last buffer)",
+          },
           ["h"] = "actions.toggle_hidden",
           ["<cr>"] = "actions.select",
           ["<c-h>"] = false,
           ["<c-v>"] = "actions.select_vsplit",
           ["<c-x>"] = "actions.select_split",
+          ["-"] = "actions.parent",
         },
         view_options = {
           show_hidden = true,
         },
       })
-
-      -- vim.keymap.set("n", "<leader>eo", function()
-      --   require("oil").toggle_float()
-      -- end, { desc = "Open the parent of the current buffer in floating window (using oil)" })
 
       vim.keymap.set("n", "<leader>eo", function()
         require("oil").open()
