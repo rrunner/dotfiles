b7bb63dfeb7ddbf4572389bb6ef1d0da7184e017
rrunner <therainbowrunner@gmail.com>
2024-12-14 15:44:31 +0100

    replace zen plugin with zen provided by Snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -32,6 +32,35 @@ return {
         },
         refresh = 20,
       },
+      zen = {
+        enabled = true,
+        toggles = {
+          dim = false,
+          git_signs = true,
+          diagnostics = true,
+          inlay_hints = false,
+        },
+        show = {
+          statusline = true,
+        },
+        on_open = function(win)
+          -- increase window width for DAP repl buffers
+          local ftypes = { "dap-repl" }
+          if vim.tbl_contains(ftypes, vim.bo.filetype) then
+            vim.api.nvim_win_set_width(win, 150)
+          end
+        end,
+        on_close = function()
+          local utils = require("config.utils")
+
+          if utils.is_debugger_running() then
+            local exists_dapui, dapui = pcall(require, "dapui")
+            if exists_dapui then
+              dapui.open({ reset = true })
+            end
+          end
+        end,
+      },
       styles = {
         notification = {
           wo = {
@@ -43,9 +72,17 @@ return {
             height = 0.9,
             keys = { q = "close" },
           },
+          zen = {
+            backdrop = 1,
+            width = 0.75,
+            height = 1,
+          },
         },
       },
     }
     require("snacks").setup(opts)
+    vim.keymap.set("n", "<leader>z", function()
+      Snacks.zen()
+    end, { noremap = true, silent = true })
   end,
 }
