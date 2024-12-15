81b659f82e7f27f571e31259f4d79f8ac8952a98
rrunner <therainbowrunner@gmail.com>
2024-12-15 15:21:16 +0100

    fix shelloptions for pwsh (windows)

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -143,7 +143,7 @@ if utils.IS_WIN then
   vim.opt.shellcmdflag =
     "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
   vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-  vim.opt.sfellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+  vim.opt.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
   vim.opt.shellquote = ""
   vim.opt.shellxquote = ""
   vim.opt.shellslash = true
