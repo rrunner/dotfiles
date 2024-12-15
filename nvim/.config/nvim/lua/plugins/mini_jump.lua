946eeb5d83236b0d73c5448badbd8214468c4d26
rrunner <therainbowrunner@gmail.com>
2024-12-15 21:41:08 +0100

    replace plugin improved-ft with mini.jump

---
+++ nvim/.config/nvim/lua/plugins/mini_jump.lua
@@ -0,0 +1,18 @@
+return {
+  "echasnovski/mini.jump",
+  version = false,
+  config = function()
+    require("mini.jump").setup({
+      mappings = {
+        forward = "f",
+        backward = "F",
+        forward_till = "t",
+        backward_till = "T",
+        repeat_jump = "",
+      },
+      delay = {
+        highlight = 10 ^ 7,
+      },
+    })
+  end,
+}
