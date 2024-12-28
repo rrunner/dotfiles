6092df024a75f83fcf2dd156d0718dd16d38e6cc
rrunner <therainbowrunner@gmail.com>
2024-12-28 12:28:51 +0100

    replace mini.jump plugin with improvedft

---
+++ nvim/.config/nvim/lua/plugins/improvedft.lua
@@ -0,0 +1,8 @@
+return {
+  "chrisbra/improvedft",
+  config = function()
+    vim.g.ft_improved_ignorecase = 1
+    vim.g.ft_improved_multichars = 0
+    vim.g.ft_improved_nohighlight = 1
+  end,
+}
