dfe59c4ec9d623d4fea0667dd09aa84a1f152362
rrunner <therainbowrunner@gmail.com>
2025-03-26 21:20:32 +0100

    update aerial config

--- nvim/.config/nvim/lua/plugins/aerial.lua
+++ nvim/.config/nvim/lua/plugins/aerial.lua
@@ -1,6 +1,6 @@
 return {
   "stevearc/aerial.nvim",
-  -- no lazy load since the aerial plugin is effectively loaded at startup because of lualine configuration
+  -- no lazy load since the aerial plugin is effectively loaded at startup because of lualine integration configuration
   config = function()
     require("aerial").setup({
       show_guides = true,
@@ -11,21 +11,27 @@ return {
       },
       disable_max_lines = 20000,
       -- filter_kind = false, -- display all symbols
-      -- close_automatic_events = { "unfocus", "switch_buffer", "unsupported" },
-      close_automatic_events = {},
+      highlight_closest = false,
       highlight_on_jump = 700,
       highlight_on_hover = true,
       post_jump_cmd = "normal! zt",
       lazy_load = false,
       close_on_select = false,
+      manage_folds = true, -- align folds between buffer and aerial
+      link_folds_to_tree = true,
+      link_tree_to_folds = true,
       keymaps = {
         ["<c-v>"] = "actions.jump_vsplit",
         ["<c-x>"] = "actions.jump_split",
         ["<c-n>"] = "actions.down_and_scroll",
         ["<c-p>"] = "actions.up_and_scroll",
         ["<c-c>"] = "actions.close",
-        ["<"] = "actions.tree_decrease_fold_level",
-        [">"] = "actions.tree_increase_fold_level",
+        ["o"] = "actions.tree_toggle",
+        ["O"] = "actions.tree_toggle_recursive",
+        ["l"] = "actions.tree_open",
+        ["L"] = "actions.tree_open_recursive",
+        ["h"] = "actions.tree_close",
+        ["H"] = "actions.tree_close_recursive",
       },
       icons = icons._kinds_cmp,
     })
