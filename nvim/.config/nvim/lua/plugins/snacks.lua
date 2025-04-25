f73df0401d72722b6b5f527c621964ff41aba16b
rrunner <therainbowrunner@gmail.com>
2025-04-25 22:07:34 +0200

    add utils to print dumps/backtrace

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -45,6 +45,16 @@ return {
     local snacks = require("snacks")
     local icons = require("config.icons")
     local utils = require("config.utils")
+
+    -- dd() - pretty printed dump of objects and pretty backtrace
+    _G.dd = function(...)
+      snacks.debug.inspect(...)
+    end
+    _G.bt = function()
+      snacks.debug.backtrace()
+    end
+    vim.print = _G.dd
+
     local opts = {
       bigfile = {
         enabled = true,
