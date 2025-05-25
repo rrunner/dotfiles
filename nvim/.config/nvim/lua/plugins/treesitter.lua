a3b1ae5f1bdd588fa1f789491489b905fddd42dc
rrunner <therainbowrunner@gmail.com>
2025-05-25 10:05:52 +0200

    not lazy load treesitter plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     "nvim-treesitter/nvim-treesitter-textobjects",
     "nvim-treesitter/nvim-treesitter-context",
   },
+  lazy = false,
   build = ":TSUpdate",
   config = function()
     local configs = require("nvim-treesitter.configs")
