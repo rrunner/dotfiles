eb4adf0977086d1f3ffe62ff7a3056a623bd03b2
rrunner <therainbowrunner@gmail.com>
2025-05-26 22:00:43 +0200

    no columns settings for tex files

--- nvim/.config/nvim/after/ftplugin/tex.lua
+++ nvim/.config/nvim/after/ftplugin/tex.lua
@@ -3,5 +3,4 @@ vim.opt_local.textwidth = 0
 vim.opt_local.wrapmargin = 0
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
-vim.opt_local.columns = 120
 vim.opt_local.colorcolumn = ""
--- vim/.vim/after/ftplugin/tex.vim
+++ vim/.vim/after/ftplugin/tex.vim
@@ -3,5 +3,4 @@ setlocal textwidth=0
 setlocal wrapmargin=0
 setlocal wrap
 setlocal linebreak
-setlocal columns=120
 setlocal colorcolumn=""
