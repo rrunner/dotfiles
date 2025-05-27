35ffede0823fe5bd6aba2fd724b71316c7273853
rrunner <therainbowrunner@gmail.com>
2025-05-27 21:40:01 +0200

    taplo LSP configuration for TOML files

--- nvim/.config/nvim/lsp/taplo.lua
+++ nvim/.config/nvim/lsp/taplo.lua
@@ -2,6 +2,18 @@ local utils = require("config.utils")
 
 return {
   cmd = { utils.app_prio("taplo"), "lsp", "stdio" },
-  root_markers = { ".toml" },
+  root_markers = { ".taplo.toml", "taplo.toml", ".git" },
   filetypes = { "toml" },
+  settings = {
+    evenBetterToml = {
+      include = {
+        paths = { "**/*.toml" },
+      },
+      formatter = {
+        compactEntries = false,
+        allowedBlankLines = 1,
+      },
+    },
+  },
+  workspace_required = false, -- single file support
 }
