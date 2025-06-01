c43414859c6a060707fcf0227f08e3b621e31070
rrunner <therainbowrunner@gmail.com>
2025-06-01 10:34:23 +0200

    start in insert mode after pasting in DAP repl buffer

--- nvim/.config/nvim/after/ftplugin/dap-repl.lua
+++ nvim/.config/nvim/after/ftplugin/dap-repl.lua
@@ -3,6 +3,7 @@ local function add_line(line)
   local lnum = vim.fn.line("$")
   line = string.gsub(line, "\n", "")
   vim.api.nvim_buf_set_lines(0, lnum - 1, lnum, true, { "dap> " .. line })
+  vim.cmd.startinsert()
 end
 
 -- use p to paste into dap-repl buffer
