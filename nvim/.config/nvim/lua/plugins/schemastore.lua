33e2d93bf97f4ee432816a8ef1f2faf1248c2d06
rrunner <therainbowrunner@gmail.com>
2025-06-01 09:31:56 +0200

    lazy load schemastore plugin

--- nvim/.config/nvim/lua/plugins/schemastore.lua
+++ nvim/.config/nvim/lua/plugins/schemastore.lua
@@ -1,4 +1,10 @@
 -- plugin that provides the SchemaStore catalog for use with jsonls and yamlls
+utils = require("config.utils")
+
 return {
   "b0o/schemastore.nvim",
+  cond = function()
+    return not utils.IS_GITHUB_BLOCKED
+  end,
+  ft = { "json", "jsonc", "yaml", "yaml.docker-compose", "yaml.gitlab" },
 }
