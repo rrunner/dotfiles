64b6a6619144b2d03a6c9d6ce5a460cb764e4cd0
rrunner <therainbowrunner@gmail.com>
2025-06-01 12:38:59 +0200

    improve behavior (keymaps) in dap-repl buffer

--- nvim/.config/nvim/after/ftplugin/dap-repl.lua
+++ nvim/.config/nvim/after/ftplugin/dap-repl.lua
@@ -1,12 +1,16 @@
 -- dap-repl specific settings
+
+-- use p to paste into dap-repl buffer
 local function add_line(line)
   local lnum = vim.fn.line("$")
   line = string.gsub(line, "\n", "")
   vim.api.nvim_buf_set_lines(0, lnum - 1, lnum, true, { "dap> " .. line })
-  vim.cmd.startinsert()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("$a", true, true, true), "n", true)
 end
 
--- use p to paste into dap-repl buffer
 vim.keymap.set("n", "p", function()
   add_line(vim.fn.getreg('"'))
 end, { buffer = 0 })
+
+-- return to normal mode after executing in dap-repl buffer
+vim.keymap.set("i", "<cr>", "<cr><esc>", { buffer = 0, noremap = true, silent = true })
