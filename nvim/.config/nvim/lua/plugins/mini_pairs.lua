6ae5f5385a83aa7648c4bfa742f2f93348e37b05
rrunner <therainbowrunner@gmail.com>
2025-06-17 19:47:32 +0200

    update mini.pairs config

--- nvim/.config/nvim/lua/plugins/mini_pairs.lua
+++ nvim/.config/nvim/lua/plugins/mini_pairs.lua
@@ -1,9 +1,11 @@
 -- autopairs
 return {
   "echasnovski/mini.pairs",
-  event = "InsertEnter",
+  event = "VeryLazy",
   version = false,
   config = function()
-    require("mini.pairs").setup()
+    require("mini.pairs").setup({
+      modes = { insert = true, command = true, terminal = false },
+    })
   end,
 }
