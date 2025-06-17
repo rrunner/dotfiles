1912c629c984eb3d23f948ad3d9f329e01d8a91b
rrunner <therainbowrunner@gmail.com>
2025-06-17 20:03:33 +0200

    better pairs for python docstrings

--- nvim/.config/nvim/lua/plugins/mini_pairs.lua
+++ nvim/.config/nvim/lua/plugins/mini_pairs.lua
@@ -6,6 +6,10 @@ return {
   config = function()
     require("mini.pairs").setup({
       modes = { insert = true, command = true, terminal = false },
+      mappings = {
+        -- better for python docstrings
+        ['"'] = { action = "closeopen", pair = '""', neigh_pattern = '[^\\"].', register = { cr = false } },
+      },
     })
   end,
 }
