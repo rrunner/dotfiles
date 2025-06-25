8607a86bf9029f11cce03430655dfad4eb9a0e5d
rrunner <therainbowrunner@gmail.com>
2025-06-25 20:58:52 +0200

    remove unneeded backtrace print function

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -45,10 +45,6 @@ vim.opt.termguicolors = true
 _G.dd = function(...)
   Snacks.debug.inspect(...)
 end
-_G.bt = function()
-  Snacks.debug.backtrace()
-end
-vim.print = _G.dd
 
 require("config.icons")
 require("config.autocmds")
