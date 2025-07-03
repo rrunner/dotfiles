7b6a8b137ddc244de73e326b9a7d0bb10eeceb8f
rrunner <therainbowrunner@gmail.com>
2025-07-03 18:37:57 +0200

    remove a few disabled plugin

--- nvim/.config/nvim/lua/plugins/disabled/improvedft_vim.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "chrisbra/improvedft",
-  config = function()
-    vim.g.ft_improved_ignorecase = 1
-    vim.g.ft_improved_multichars = 0
-    vim.g.ft_improved_nohighlight = 1
-  end,
-}
