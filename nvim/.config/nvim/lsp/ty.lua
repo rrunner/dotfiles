ba02adbcfc45e9ba9456b0b23395a1c9d2d639ba
rrunner <therainbowrunner@gmail.com>
2025-08-14 20:42:45 +0200

    update ty LSP config

--- nvim/.config/nvim/lsp/ty.lua
+++ nvim/.config/nvim/lsp/ty.lua
@@ -4,17 +4,20 @@ return {
   cmd = { utils.app_prio("ty"), "server" },
   root_markers = vim.g.py_root_markers,
   filetypes = { "python" },
-  init_options = {
-    settings = {
-      python = {
-        ty = {
-          disableLanguageServices = true,
-        },
+  settings = {
+    init_options = {
+      -- logFile = "~/ty.log",
+      -- logLevel = "debug",
+    },
+    ty = {
+      disableLanguageServices = true,
+      diagnosticMode = "workspace",
+      inlayHints = {
+        variableTypes = false,
+        callArgumentNames = false,
       },
-      ty = {
-        diagnosticMode = "workspace",
-        -- logFile = "~/ty.log",
-        -- logLevel = "debug",
+      experimental = {
+        rename = false,
       },
     },
   },
