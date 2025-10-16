af22fb0d997e67c1bcff594d697b8ffdc6aa0805
rrunner <therainbowrunner@gmail.com>
2025-10-16 22:46:09 +0200

    hide the window separator

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -25,19 +25,22 @@ return {
     priority = 1000,
     config = function()
       require("conifer").setup({
-        variant = "lunar", -- "solar" for light
+        variant = "lunar", -- "lunar" for dark, "solar" for light
         transparent = false, -- whether to set the bg color for the lunar variant
         styles = {
           comments = {},
           functions = {},
           keywords = {},
           lsp = {},
-          match_paren = { underline = false, link = "Visual" },
+          match_paren = { underline = false, link = "Title" },
           type = { bold = false },
           variables = {},
         },
       })
       vim.cmd.colorscheme("conifer")
+      -- hide the win separator
+      color = vim.o.background == "dark" and "#161716" or "#E8E8C2"
+      vim.api.nvim_set_hl(0, "WinSeparator", { bg = color, fg = color })
     end,
   },
 }
