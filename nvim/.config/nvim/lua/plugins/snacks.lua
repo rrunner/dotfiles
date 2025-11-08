382c871d41225c61c739ae2d0a47262d821eb86e
rrunner <therainbowrunner@gmail.com>
2025-11-09 00:34:22 +0100

    use required module

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -142,7 +142,7 @@ return {
                 "<c-enter>",
                 function(self)
                   local name = "scratch." .. vim.fn.fnamemodify(vim.api.nvim_buf_get_name(self.buf), ":e")
-                  Snacks.debug.run({ buf = self.buf, name = name })
+                  snacks.debug.run({ buf = self.buf, name = name })
                 end,
                 desc = "Source buffer",
                 mode = { "n", "x" },
