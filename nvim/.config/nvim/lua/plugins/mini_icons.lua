427642577c91ef3d06a36b561c13b757f3e75a37
rrunner <therainbowrunner@gmail.com>
2025-12-11 12:20:20 +0100

    add icons for blink-cmp-words sources

--- nvim/.config/nvim/lua/plugins/mini_icons.lua
+++ nvim/.config/nvim/lua/plugins/mini_icons.lua
@@ -2,6 +2,12 @@ return {
   "nvim-mini/mini.icons",
   version = false,
   config = function()
-    require("mini.icons").setup()
+    require("mini.icons").setup({
+      lsp = {
+        -- icons to represent blink-cmp-words completion items
+        dictionary = { glyph = '󰍉 ' },
+        thesaurus = { glyph = ' ' },
+      },
+    })
   end,
 }
