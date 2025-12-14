279ea689d1dff320bbe67f0d8f9372910992d829
rrunner <therainbowrunner@gmail.com>
2025-12-14 12:24:07 +0100

    remove conifer color scheme

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -20,30 +20,6 @@ return {
     end,
   },
   {
-    "lucasadelino/conifer.nvim",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      require("conifer").setup({
-        variant = "lunar",   -- "lunar" for dark, "solar" for light
-        transparent = false, -- whether to set the bg color for the lunar variant
-        styles = {
-          comments = {},
-          functions = {},
-          keywords = {},
-          lsp = {},
-          match_paren = { underline = false, link = "Title" },
-          type = { bold = false },
-          variables = {},
-        },
-      })
-      vim.cmd.colorscheme("conifer")
-      -- hide the win separator
-      local color = vim.o.background == "dark" and "#161716" or "#E8E8C2"
-      vim.api.nvim_set_hl(0, "WinSeparator", { bg = color, fg = color })
-    end,
-  },
-  {
     "gbprod/nord.nvim",
     lazy = false,
     priority = 1000,
