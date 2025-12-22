c38024c4e94e464627ec5d8ea0340da2b0332b85
rrunner <therainbowrunner@gmail.com>
2025-12-22 19:43:49 +0100

    delete nightfox color scheme incl. alacritty themes

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -2,24 +2,6 @@
 -- set lazy=true for all to use default colorscheme
 return {
   {
-    "EdenEast/nightfox.nvim",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      require("nightfox").setup({
-        options = {
-          styles = {
-            comments = "italic",
-            keywords = "bold",
-            types = "italic,bold",
-          },
-        },
-      })
-      -- available themes: nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
-      vim.cmd.colorscheme("nordfox")
-    end,
-  },
-  {
     "gbprod/nord.nvim",
     lazy = false,
     priority = 1000,
