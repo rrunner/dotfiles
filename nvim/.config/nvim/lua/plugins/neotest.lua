c3f9dc82814f82b24ab40a93a8ad202b7a9afce3
rrunner <therainbowrunner@gmail.com>
2026-01-04 21:36:14 +0100

    add neotest adapter for Elixir

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -9,8 +9,9 @@ return {
     -- adapters
     "nvim-neotest/neotest-python",
     "shunsambongi/neotest-testthat",
+    "jfpedroza/neotest-elixir",
   },
-  ft = { "python", "r" },
+  ft = { "python", "r", "elixir" },
   config = function()
     require("neotest").setup({
       icons = {
@@ -31,6 +32,7 @@ return {
           end,
         }),
         require("neotest-testthat"),
+        require("neotest-elixir"),
       },
       summary = {
         mappings = {
@@ -40,55 +42,53 @@ return {
       },
     })
 
-    vim.keymap.set("n", "<leader>tf",
-      function()
-        require("neotest").run.run(vim.fn.expand("%"))
-      end,
-      {
-        noremap = true,
-        silent = true,
-        desc = "Run tests for the current file",
-      })
+    vim.keymap.set("n", "<leader>tf", function()
+      require("neotest").run.run(vim.fn.expand("%"))
+    end, {
+      noremap = true,
+      silent = true,
+      desc = "Run tests for the current file",
+    })
 
-    vim.keymap.set("n", "<leader>tn",
-      function()
-        require("neotest").run.run()
-      end,
-      {
-        noremap = true,
-        silent = true,
-        desc = "Run the nearest test",
-      })
+    vim.keymap.set("n", "<leader>tn", function()
+      require("neotest").run.run()
+    end, {
+      noremap = true,
+      silent = true,
+      desc = "Run the nearest test",
+    })
 
-    vim.keymap.set("n", "<leader>tq",
-      function()
+    vim.keymap.set("n", "<leader>tq", function()
+      if vim.bo.filetype == "python" then
         require("dap").toggle_breakpoint()
         require("neotest").run.run({ strategy = "dap" })
-      end,
-      {
-        noremap = true,
-        silent = true,
-        desc = "Quicker debug the nearest test (set breakpoint and run)",
-      })
+      else
+        vim.notify("Debugging test not configured", "warn")
+      end
+    end, {
+      noremap = true,
+      silent = true,
+      desc = "Quicker debug the nearest test (set breakpoint and run)",
+    })
 
-    vim.keymap.set("n", "<leader>td",
-      function()
+    vim.keymap.set("n", "<leader>td", function()
+      if vim.bo.filetype == "python" then
         require("neotest").run.run({ strategy = "dap" })
-      end,
-      {
-        noremap = true,
-        silent = true,
-        desc = "Debug the nearest test",
-      })
+      else
+        vim.notify("Debugging test not configured", "warn")
+      end
+    end, {
+      noremap = true,
+      silent = true,
+      desc = "Debug the nearest test",
+    })
 
-    vim.keymap.set("n", "<leader>tt",
-      function()
-        require("neotest").summary.toggle()
-      end,
-      {
-        noremap = true,
-        silent = true,
-        desc = "Toggle the neotest summary window",
-      })
+    vim.keymap.set("n", "<leader>tt", function()
+      require("neotest").summary.toggle()
+    end, {
+      noremap = true,
+      silent = true,
+      desc = "Toggle the neotest summary window",
+    })
   end,
 }
