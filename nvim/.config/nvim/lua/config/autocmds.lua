78d875c9579104c778db26cbbf05c53da3174305
rrunner <therainbowrunner@gmail.com>
2026-03-22 11:07:59 +0100

    remove autocommand to keep terminla bg color in sync with nvim bg color

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -266,19 +266,6 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown", "quarto", "rmd" },
 })
 
--- keep terminal background's color in sync with Neovim's background color using OSC control sequences (https://github.com/neovim/neovim/issues/16572#issuecomment-1954420136)
-local modified = false
-vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
-  callback = function()
-    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
-    if normal.bg then
-      io.write(string.format("\027]11;#%06x\027\\", normal.bg))
-      modified = true
-    end
-  end,
-  group = config,
-})
-
 -- fix conceallevel for json files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
