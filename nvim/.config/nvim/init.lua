9ce09f4ff48956c14a3c36ab341fe3342de82f9e
rrunner <therainbowrunner@gmail.com>
2026-04-06 20:51:43 +0200

    adjust rtp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -45,7 +45,6 @@ require("lazy").setup("plugins", {
   },
   performance = {
     rtp = {
-      paths = { vim.fn.stdpath("data") .. "/site/" },
       disabled_plugins = {
         "gzip",
         "matchit",
