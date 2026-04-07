6121ad95f9e43ef808617bc484098e518a7d9b9c
rrunner <therainbowrunner@gmail.com>
2026-04-07 20:55:02 +0200

    use built-in plugins undotree/difftool

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,12 @@
--- byte-compile and cache lua files
+-- byte-compile and cache lua files/modules
 vim.loader.enable()
 
 -- define global configuration table
 _G.Config = {}
 
--- use new exploratory UI
+-- use exploratory UI
 require("vim._core.ui2").enable({})
+
+-- use built-in plugins
+vim.cmd.packadd("nvim.undotree") -- :Undotree
+vim.cmd.packadd("nvim.difftool") -- :Difftool <folder1|file1> <folder2|file2>
